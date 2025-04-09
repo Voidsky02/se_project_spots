2b228276a1d23a6c6c17b824f9445ade3c20231f
voidsky <voidsky02@gmail.com>
2025-04-08 20:32:15 -0700

    restarted project

--- scripts/validation.js
+++ scripts/validation.js
@@ -6,37 +6,3 @@ const settings = {
   inputErrorClass: "modal__input_type_error",
   errorClass: "modal__error_visible",
 };
-
-// const formList = Array.from(document.querySelectorAll('.modal__form'));
-
-function showInputError(formElement) {
-  formElement.classList.add(settings.errorClass);
-}
-
-function hideInputError(formElement) {
-  formElement.classList.remove(settings.errorClass);
-}
-
-function checkInputValidity(config) {}
-
-function setEventListeners(formElement, config) {
-  // dont know about this code below
-  const buttonElement = formElement.querySelector(config.submitButtonSelector);
-  buttonElement.addEventListener(submit, (event) => {
-    event.preventDefault();
-  });
-
-  const inputElement = formElement.querySelector(settings.inputSelector);
-  inputElement.addEventListener(input, checkInputValidity);
-}
-
-// below function will automatically add validation to new elements
-function enableValidation(config) {
-  const formList = document.querySelectorAll(settings.formSelector);
-
-  formList.forEach((formEl) => {
-    setEventListeners(formEl, settings);
-  });
-}
-// above code must prevent default submit events automatically &
-// add input event listeners to every input element in a form automatically
