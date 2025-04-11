b22c181c8cc1ed5311ef84327462d3ffe9dc77c6
voidsky <voidsky02@gmail.com>
2025-04-11 01:46:35 -0700

    rought draft of videos 1-3 / realized i never linked the script when doing it on my own, will continue with videos anyways cause project late

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,8 +1,44 @@
-const settings = {
-  formSelector: ".modal__form",
-  inputSelector: ".modal__input",
-  submitButtonSelector: ".modal__submit-btn",
-  inactiveButtonClass: "modal__submit-btn_disabled",
-  inputErrorClass: "modal__input_type_error",
-  errorClass: "modal__error_visible",
+// const settings = {
+//   formSelector: ".modal__form",
+//   inputSelector: ".modal__input",
+//   submitButtonSelector: ".modal__submit-btn",
+//   inactiveButtonClass: "modal__submit-btn_disabled",
+//   inputErrorClass: "modal__input_type_error",
+//   errorClass: "modal__error_visible",
+// };
+
+const showInputError = (formEl, inputEl, errorMsg) => {
+  const errorMsgID = inputEl.id + "-error";
+  const errorMsgEl = document.querySelector(`#${inputEl.id}-error`);
+  errorMsgEl.textContent = errorMsg;
 };
+
+const checkInputValidity = (formEl, inputEl) => {
+  if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, inputEl.validationMessage);
+  }
+};
+
+const setEventListeners = (formEl) => {
+  const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
+  const buttonElement = formEl.querySelector(".modal__submit-btn");
+
+  // TODO - Handle initial states
+  // toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formEl, inputElement);
+      // toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = document.querySelectorAll(".modal__form");
+  formList.forEach((formEl) => {
+    setEventListeners(formEl);
+  });
+};
+
+enableValidation();
